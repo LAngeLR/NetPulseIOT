aa5af42f1a45dff589b45a992679868ad611c6d3
oscaragredav <oscar@itic.com.pe>
2024-04-18 14:46:02 -0500

    linkeado sitio

--- app/src/main/java/com/example/netpulseiot/AdminActivity.java
+++ app/src/main/java/com/example/netpulseiot/AdminActivity.java
@@ -1,6 +1,8 @@
 package com.example.netpulseiot;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,7 +11,13 @@ public class AdminActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin2);
+        setContentView(R.layout.activity_admin1);
 
     }
+
+    public void listaSitio(View view){
+        Intent intent = new Intent(this, AdminListaSitiosActivity.class);
+        startActivity(intent);
+    }
+
 }
