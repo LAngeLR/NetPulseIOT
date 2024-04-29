9ada39038be5a511c6138b87e7c412f2c96c07fc
RommelGaray <a20200241@pucp.edu.pe>
2024-04-29 00:00:38 -0500

    Menu inferior Superadmin

--- app/src/main/java/com/example/netpulseiot/fragmentos/superadmin/SuperadminActivity.java
+++ app/src/main/java/com/example/netpulseiot/fragmentos/superadmin/SuperadminActivity.java
@@ -1,5 +1,6 @@
 package com.example.netpulseiot.fragmentos.superadmin;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,24 +10,67 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.netpulseiot.R;
 import com.example.netpulseiot.SuperAdminListaUsarios;
 import com.example.netpulseiot.SuperadminListaMensajes;
+import com.example.netpulseiot.databinding.ActivitySuperadminBinding;
 
 public class SuperadminActivity extends AppCompatActivity {
+
+    ActivitySuperadminBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_superadmin);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivitySuperadminBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new InicioSuperadminFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+//            switch (item.getItemId()){
+//
+//                case R.id.inicioSuperadmin:
+//                    replaceFragment(new InicioSuperadminFragment());
+//                    break;
+//
+//                case R.id.mensajesSuperadmin:
+//                    replaceFragment(new MensajesSuperadminFragment());
+//                    break;
+//
+//                case R.id.usuariosSuperadmin:
+//                    replaceFragment(new UsuariosSuperadminFragment());
+//                    break;
+//
+//            }
+
+            if (item.getItemId() == R.id.inicioSuperadmin) {
+                replaceFragment(new InicioSuperadminFragment());
+            } else if (item.getItemId() == R.id.mensajesSuperadmin) {
+                replaceFragment(new MensajesSuperadminFragment());
+            } else if (item.getItemId() == R.id.usuariosSuperadmin) {
+                replaceFragment(new InicioSuperadminFragment());
+            }
+
+            return true;
+
         });
+
     }
+
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_superadmin, fragment);
+        fragmentTransaction.commit();
+    }
+
+
 
     public void listaMensajes(View view){
         Intent intent = new Intent(this, SuperadminListaMensajes.class);
