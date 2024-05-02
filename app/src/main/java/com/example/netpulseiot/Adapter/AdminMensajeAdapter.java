049e5caaa59405e67dcd30df5b103f35f052fa2e
oscaragredav <oscar@itic.com.pe>
2024-05-02 14:02:57 -0500

    terminado Recycler de admin

---
+++ app/src/main/java/com/example/netpulseiot/Adapter/AdminMensajeAdapter.java
@@ -0,0 +1,65 @@
+package com.example.netpulseiot.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.netpulseiot.R;
+import com.example.netpulseiot.entity.AdminMensajeItem;
+
+import java.util.List;
+
+public class AdminMensajeAdapter extends RecyclerView.Adapter<AdminMensajeAdapter.AdminMensajeViewHolder>{
+
+    Context context;
+    List<AdminMensajeItem> lista;
+
+    public AdminMensajeAdapter(Context context, List<AdminMensajeItem> lista) {
+        this.context = context;
+        this.lista = lista;
+    }
+
+    @NonNull
+    @Override
+    public AdminMensajeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_admin_mensaje,parent,false);
+        return new AdminMensajeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdminMensajeViewHolder holder, int position) {
+
+        holder.nombreItem.setText(lista.get(position).getName());
+        holder.mensajeItem.setText(lista.get(position).getMensaje());
+        holder.fotoItem.setImageResource(lista.get(position).getImage());
+        holder.contadorItem.setText(lista.get(position).getContador());
+    }
+
+    @Override
+    public int getItemCount() {
+        return lista.size();
+    }
+
+
+    public class AdminMensajeViewHolder extends RecyclerView.ViewHolder{
+        ImageView fotoItem;
+        TextView nombreItem, mensajeItem;
+        Button contadorItem;
+        public AdminMensajeViewHolder(@NonNull View itemView) {
+            super(itemView);
+            fotoItem = itemView.findViewById(R.id.fotoItem);
+            nombreItem = itemView.findViewById(R.id.nombreItem);
+            mensajeItem = itemView.findViewById(R.id.mensajeItem);
+            contadorItem = itemView.findViewById(R.id.contadorItem);
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/netpulseiot/entity/AdminMensajeItem.java
@@ -0,0 +1,47 @@
+package com.example.netpulseiot.entity;
+
+public class AdminMensajeItem {
+    String name;
+    String mensaje;
+    int image;
+    String contador;
+
+    public AdminMensajeItem(String name, String mensaje, int image, String contador) {
+        this.name = name;
+        this.mensaje = mensaje;
+        this.image = image;
+        this.contador = contador;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getContador() {
+        return contador;
+    }
+
+    public void setContador(String contador) {
+        this.contador = contador;
+    }
+}
