4ff3b9bf144f28493143ca5ff5b0415d0240fa59
oscaragredav <oscar@itic.com.pe>
2024-05-02 16:39:02 -0500

    terminado Recycler mensaje superadmin

--- app/src/main/java/com/example/netpulseiot/Adapter/SuperadminMensajeAdapter.java
+++ app/src/main/java/com/example/netpulseiot/Adapter/SuperadminMensajeAdapter.java
@@ -1,10 +1,64 @@
 package com.example.netpulseiot.Adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class SuperadminMensajeAdapter {
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.netpulseiot.R;
+import com.example.netpulseiot.entity.SuperadminMensajeItem;
+
+import java.util.List;
+
+public class SuperadminMensajeAdapter extends RecyclerView.Adapter<SuperadminMensajeAdapter.SuperadminMensajeViewHolder> {
 
     Context context;
-    
+    List<SuperadminMensajeItem> lista;
 
+    public SuperadminMensajeAdapter(Context context, List<SuperadminMensajeItem> lista) {
+        this.context = context;
+        this.lista = lista;
+    }
+
+    @NonNull
+    @Override
+    public SuperadminMensajeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_superadmin_mensaje,parent,false);
+
+        return new SuperadminMensajeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SuperadminMensajeViewHolder holder, int position) {
+        holder.nombreItem.setText(lista.get(position).getName());
+        holder.mensajeItem.setText(lista.get(position).getMensaje());
+        holder.fotoItem.setImageResource(lista.get(position).getImage());
+        holder.contadorItem.setText(lista.get(position).getContador());
+    }
+
+    @Override
+    public int getItemCount() {
+        return lista.size();
+    }
+
+    public class SuperadminMensajeViewHolder extends RecyclerView.ViewHolder{
+        ImageView fotoItem;
+        TextView nombreItem, mensajeItem;
+        Button contadorItem;
+
+        public SuperadminMensajeViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            fotoItem = itemView.findViewById(R.id.fotoItem);
+            nombreItem = itemView.findViewById(R.id.nombreItem);
+            mensajeItem = itemView.findViewById(R.id.mensajeItem);
+            contadorItem = itemView.findViewById(R.id.contadorItem);
+        }
+    }
 }
