ba21ad50a64c9c4dc8a780b949458c52c96f0218
LuisAngel <a20192832@pucp.edu.pe>
2024-06-23 13:36:16 -0500

    cerrado de sesion para probar sin inconvenientes

--- app/src/main/java/com/example/netpulseiot/AdminActivity.java
+++ app/src/main/java/com/example/netpulseiot/AdminActivity.java
@@ -2,29 +2,69 @@ package com.example.netpulseiot;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.netpulseiot.databinding.ActivityAdmin1Binding;
+import com.example.netpulseiot.databinding.ActivitySuperadminBinding;
+import com.example.netpulseiot.fragmentos.UsersFragment;
 import com.example.netpulseiot.fragmentos.admin.AdminEquiposFragment;
 import com.example.netpulseiot.fragmentos.admin.AdminInicioFragment;
 import com.example.netpulseiot.fragmentos.admin.AdminListaMensajesFragmenmt;
 import com.example.netpulseiot.fragmentos.admin.AdminSitiosFragment;
 import com.example.netpulseiot.fragmentos.admin.AdminUsuariosFragment;
+import com.example.netpulseiot.fragmentos.superadmin.HistorialSuperadminFragment;
+import com.example.netpulseiot.fragmentos.superadmin.InicioSuperadminFragment;
+import com.example.netpulseiot.fragmentos.superadmin.SuperadminActivity;
+import com.example.netpulseiot.fragmentos.superadmin.UsuariosSuperadminFragment;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.time.Instant;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class AdminActivity extends AppCompatActivity {
 
     ActivityAdmin1Binding binding;
+
+    FirebaseUser firebaseUser;
+    DatabaseReference reference;
+
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityAdmin1Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("NetPulse");
+
+        /** CREAR INSTANCIA DE BD FIREBASE **/
+        db = FirebaseFirestore.getInstance();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+
+
         replaceFragment(new AdminInicioFragment());
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
@@ -41,10 +81,28 @@ public class AdminActivity extends AppCompatActivity {
                 replaceFragment(new AdminEquiposFragment());
             }
 
+            int selectedItemId = item.getItemId();
+            changeSelectedMenuItemTextAppearance(selectedItemId);
+
             return true;
-
         });
+    }
 
+    private void changeSelectedMenuItemTextAppearance(int selectedItemId) {
+        Menu menu = binding.bottomNavigationView.getMenu();
+        for (int i = 0; i < menu.size(); i++) {
+            MenuItem menuItem = menu.getItem(i);
+            boolean isSelected = menuItem.getItemId() == selectedItemId;
+
+            // Change text appearance based on selection (using SpannableString)
+            if (isSelected) {
+                menuItem.setTitle(menuItem.getTitle().toString().replaceFirst("[\\p{Cntrl}]", "").replaceFirst("[\\p{Punct}]", "")); // Remove special characters
+                menuItem.setTitle(Html.fromHtml("<font color='#FFFFFF'>" + menuItem.getTitle() + "</font>")); // Set text content with color
+            } else {
+                // Set default text appearance (optional)
+                menuItem.setTitle(menuItem.getTitle().toString()); // Reset text (optional)
+            }
+        }
     }
 
     public void replaceFragment(Fragment fragment){
@@ -88,6 +146,23 @@ public class AdminActivity extends AppCompatActivity {
     public void mensaje(View view){
         Intent intent = new Intent(this, AdminMensajeActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.tercero) { // ID del ítem "Cerrar sesión"
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(AdminActivity.this, MainActivity.class));
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
--- app/src/main/java/com/example/netpulseiot/SupervisorActivity.java
+++ app/src/main/java/com/example/netpulseiot/SupervisorActivity.java
@@ -2,28 +2,60 @@ package com.example.netpulseiot;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.example.netpulseiot.databinding.ActivityAdmin1Binding;
 import com.example.netpulseiot.databinding.ActivitySupervisorBinding;
+import com.example.netpulseiot.fragmentos.admin.AdminEquiposFragment;
+import com.example.netpulseiot.fragmentos.admin.AdminInicioFragment;
+import com.example.netpulseiot.fragmentos.admin.AdminListaMensajesFragmenmt;
+import com.example.netpulseiot.fragmentos.admin.AdminSitiosFragment;
+import com.example.netpulseiot.fragmentos.admin.AdminUsuariosFragment;
 import com.example.netpulseiot.fragmentos.supervisor.SupervisorEquiposFragment;
 import com.example.netpulseiot.fragmentos.supervisor.SupervisorInicioFragment;
 import com.example.netpulseiot.fragmentos.supervisor.SupervisorMensajesFragment;
 import com.example.netpulseiot.fragmentos.supervisor.SupervisorSitiosFragment;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SupervisorActivity extends AppCompatActivity {
 
     ActivitySupervisorBinding binding;
+
+    FirebaseUser firebaseUser;
+    DatabaseReference reference;
+
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySupervisorBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("NetPulse");
+
+        /** CREAR INSTANCIA DE BD FIREBASE **/
+        db = FirebaseFirestore.getInstance();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+
+
         replaceFragment(new SupervisorInicioFragment());
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
@@ -38,10 +70,28 @@ public class SupervisorActivity extends AppCompatActivity {
                 replaceFragment(new SupervisorEquiposFragment());
             }
 
+            int selectedItemId = item.getItemId();
+            changeSelectedMenuItemTextAppearance(selectedItemId);
+
             return true;
-
         });
+    }
 
+    private void changeSelectedMenuItemTextAppearance(int selectedItemId) {
+        Menu menu = binding.bottomNavigationView.getMenu();
+        for (int i = 0; i < menu.size(); i++) {
+            MenuItem menuItem = menu.getItem(i);
+            boolean isSelected = menuItem.getItemId() == selectedItemId;
+
+            // Change text appearance based on selection (using SpannableString)
+            if (isSelected) {
+                menuItem.setTitle(menuItem.getTitle().toString().replaceFirst("[\\p{Cntrl}]", "").replaceFirst("[\\p{Punct}]", "")); // Remove special characters
+                menuItem.setTitle(Html.fromHtml("<font color='#FFFFFF'>" + menuItem.getTitle() + "</font>")); // Set text content with color
+            } else {
+                // Set default text appearance (optional)
+                menuItem.setTitle(menuItem.getTitle().toString()); // Reset text (optional)
+            }
+        }
     }
 
     public void replaceFragment(Fragment fragment){
@@ -85,5 +135,22 @@ public class SupervisorActivity extends AppCompatActivity {
     public void mensaje(View view){
         Intent intent = new Intent(this, AdminMensajeActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.tercero) { // ID del ítem "Cerrar sesión"
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(SupervisorActivity.this, MainActivity.class));
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
